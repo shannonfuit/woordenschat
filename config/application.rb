72fc67264f1e4a37097e86666e5163f1775fd705
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-22 19:29:38 +0200

    undo of the fuckup

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(*Rails.groups)
 
 module Woordenschat2
   class Application < Rails::Application
@@ -21,4 +21,3 @@ module Woordenschat2
     # config.i18n.default_locale = :de
   end
 end
-
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-Woordenschat2::Application.initialize!
+# Initialize the Rails application.
+Rails.application.initialize!
