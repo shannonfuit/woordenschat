e7e3472ed225787cceeefc54ae395a47cc00bddd
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-24 23:06:43 +0200

    added bucket to amazon

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIXCE63QUSZU2IKMQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
-   
-  config.fog_directory  = 'taaltelt'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    :provider              => 'AWS',
+    :aws_access_key_id     => ENV['S3_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET']
+  }
+
+  config.fog_directory =  ENV['S3_BUCKET_NAME']
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
 end
