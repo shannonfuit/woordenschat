dd53f2e9971dedf3119fe6926b4709f02b326522
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-25 14:51:42 +0200

    z removed. stupid error!

--- app/uploaders/hintimage_uploader.rb
+++ app/uploaders/hintimage_uploader.rb
@@ -15,7 +15,7 @@ class HintimageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-z
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
