eecbf81268bb3cb2a7a7eca9bbd3030e99fb3a2c
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-25 13:36:27 +0200

    nieuwe ronde nieuwe kansen met Amazon. grom

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,6 +15,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
 
   def default_url
     'default.png'
