aa1f450354482f7f046b797e9c77b75e2e7f9234
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-25 13:39:01 +0200

    werkt niet, undo

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,7 +15,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
 
   def default_url
     'default.png'
