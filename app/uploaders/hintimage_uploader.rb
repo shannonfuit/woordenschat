67319f3b94ce552edf2059782cf22a0acee396b1
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-26 11:46:53 +0200

    status van gisteren met vragen 5

--- app/uploaders/hintimage_uploader.rb
+++ app/uploaders/hintimage_uploader.rb
@@ -16,6 +16,10 @@ class HintimageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+ def default_url
+    'preview.png'
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
