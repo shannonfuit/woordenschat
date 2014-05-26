e95086d10b4ed8f7ef102ec95a9eb930eada4a2d
Shannon Fuit <shannon.fuit@gmail.com>
2014-05-26 10:45:55 +0200

    test without picture 6

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -17,7 +17,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   def default_url
-    'uploads/default.png'
+    'assets/images/default.png'
  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
