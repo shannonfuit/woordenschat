eb4ae2918c096acba7b221208f678b1b907ea0f2
Shannon Fuit <shannon.fuit@gmail.com>
2016-05-19 22:38:24 +0200

    refactored NoHintsMedal

--- app/models/no_hints_medal.rb
+++ app/models/no_hints_medal.rb
@@ -1,14 +1,11 @@
 class NoHintsMedal < LevelMedal
-  def judge(user, playedlevel)
-    # normally this should obviously only apply when the level numbers are equal.
-    if playedlevel.answers.where(used_semantic_hint: true).count == 0 &&
-       playedlevel.answers.where(used_sentence_hint: true).count == 0 &&
-       playedlevel.answers.where(used_image_hint: true).count == 0 &&
-       playedlevel.level.number == level_number && !user.has_won(self)
-
-      achievement = Achievement.create(user_id: user, medal: self)
-      user.achievements.push(achievement)
-      return true
-    end
+  def judge(user, played_level)
+    answers = playedlevel.answers
+    return unless answers.find_by(used_semantic_hint: true) &&
+                  answers.find_by(used_sentence_hint: true) &&
+                  answers.find_by(used_image_hint: true) &&
+                  played_level.level.number == level_number &&
+                  !user.has_won(self)
+    user.achievements.create(user: user, medal: self)
   end
 end
