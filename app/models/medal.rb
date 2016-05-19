63d891aae42713dd5e3312a4c69b6d1a78f4ac1a
Shannon Fuit <shannon.fuit@gmail.com>
2016-05-19 20:52:21 +0200

    refactoring FiftyInRow model

--- app/models/medal.rb
+++ app/models/medal.rb
@@ -1,7 +1,6 @@
 class Medal < ActiveRecord::Base
-
-	# call to have the medal judge and apply itself when valid to the users achievements.
-	def judge(user, playedlevel)
-		raise NotImplementedError.new("You must implement this on the derived type")
-	end
+  # call to have the medal judge and apply itself when valid to the users achievements.
+  def judge(_user, _playedlevel)
+    raise NotImplementedError, 'You must implement this on the derived type'
+  end
 end
